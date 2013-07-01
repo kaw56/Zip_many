43b4a0ae91ad0e79e1dca48af58e009ef7c127aa
Katherine <kwhite@mrc-lmb.cam.ac.uk>
2013-07-01 12:32:32 +0100

    An actual, working shell script
    This code works to go through each folder in a directory and look
    for .gz files and extracts them if present.
    
    Is a little slow but I like it and I think just giving a whole
    list to gunzip is similarly slow IIRC

--- unzip.sh
+++ unzip.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 # mostly playing with shell scripting
 
-# for each directory,
-    # move into it
-    # and unzip all the relevant files
+# a list of directories to look in for
+MY_DIRECTORIES="*"
+
+for i in $MY_DIRECTORIES
+    do
+        # check if there are .gz files in the folder.
+        ls -1 $i/*.gz
+        if [ $? -eq 1 ]
+        then
+            echo ".gz files not found in $i"
+            continue
+        fi
+
+    # actually unzip!
+    gunzip $i/*.gz
+done
+
